e42430f6b915b1574614f87592f13467ed9b803e
Dave Toerber <david.toerber@broncos.nfl.net>
2026-01-01 08:24:53 -0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TreeComponent } from './components/tree/tree.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tree',
+    component: TreeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'tree',
+    pathMatch: 'full',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [NavComponent],
+  template: `<app-nav></app-nav>`,
 })
-export class App {
-  protected readonly title = signal('tree-view');
-}
+export class App {}
